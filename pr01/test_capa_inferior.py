21d06d9e201945fe903ae16bcaa736602d63e3df
OGShawnLee <OGShawnLee@gmail.com>
2026-04-19 14:45:10 -0600

    test: rename capa_inferior test cases

--- pr01/test_capa_inferior.py
+++ pr01/test_capa_inferior.py
@@ -7,51 +7,51 @@ DURACION_MESES_INVALIDO = 6
 PERFIL_ALTO_RIESGO = True
 PERFIL_BAJO_RIESGO = False
 
-def test_escenario_e1():
+def test_capa_inferior_escenario_e1():
   [inversion, errores] = calcular_monto(PERFIL_ALTO_RIESGO, CAPITAL_NEGATIVO, DURACION_MESES_INVALIDO)
 
   assert inversion == None
   assert errores["error-capital-invalido"] == ERROR_CAPITAL_INVALIDO
   assert errores["error-plazo-invalido"] == ERROR_PLAZO_INVALIDO
 
-def test_escenario_e2():
+def test_capa_inferior_escenario_e2():
   [inversion, errores] = calcular_monto(PERFIL_ALTO_RIESGO, CAPITAL_NEGATIVO, DURACION_MESES_VALIDO)
 
   assert inversion == None
   assert errores["error-capital-invalido"] == ERROR_CAPITAL_INVALIDO
 
-def test_escenario_e3():
+def test_capa_inferior_escenario_e3():
   [inversion, errores] = calcular_monto(PERFIL_ALTO_RIESGO, CAPITAL_VALIDO, DURACION_MESES_INVALIDO)
 
   assert inversion == None
   assert errores["error-plazo-invalido"] == ERROR_PLAZO_INVALIDO
 
-def test_escenario_e4():
+def test_capa_inferior_escenario_e4():
   [inversion, errores] = calcular_monto(PERFIL_ALTO_RIESGO, CAPITAL_VALIDO, DURACION_MESES_VALIDO)
 
   assert inversion == calcular_monto_alto_riesgo(CAPITAL_VALIDO, DURACION_MESES_VALIDO)
   assert errores == None
 
-def test_escenario_e5():
+def test_capa_inferior_escenario_e5():
   [inversion, errores] = calcular_monto(PERFIL_BAJO_RIESGO, CAPITAL_NEGATIVO, DURACION_MESES_INVALIDO)
 
   assert inversion == None
   assert errores["error-capital-invalido"] == ERROR_CAPITAL_INVALIDO
   assert errores["error-plazo-invalido"] == ERROR_PLAZO_INVALIDO
 
-def test_escenario_e6():
+def test_capa_inferior_escenario_e6():
   [inversion, errores] = calcular_monto(PERFIL_BAJO_RIESGO, CAPITAL_NEGATIVO, DURACION_MESES_VALIDO)
 
   assert inversion == None
   assert errores["error-capital-invalido"] == ERROR_CAPITAL_INVALIDO
 
-def test_escenario_e7():
+def test_capa_inferior_escenario_e7():
   [inversion, errores] = calcular_monto(PERFIL_BAJO_RIESGO, CAPITAL_VALIDO, DURACION_MESES_INVALIDO)
 
   assert inversion == None
   assert errores["error-plazo-invalido"] == ERROR_PLAZO_INVALIDO
 
-def test_escenario_e8():
+def test_capa_inferior_escenario_e8():
   [inversion, errores] = calcular_monto(PERFIL_BAJO_RIESGO, CAPITAL_VALIDO, DURACION_MESES_VALIDO)
 
   assert inversion == calcular_monto_bajo_riesgo(CAPITAL_VALIDO, DURACION_MESES_VALIDO)
